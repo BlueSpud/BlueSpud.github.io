bde41cfccb29db06fea58fdf667e085758e5a978
Logan <logan2299@yahoo.com>
2018-02-26 13:00:54 -0500

    Updated routes banner

--- script/carousel.js
+++ script/carousel.js
@@ -5,7 +5,7 @@ $(function() {
     
     var step = 100 / size;
     
-    var colors = ["#D73131", "black", "#FA1159", "#FF162E", "#0068FF", "#2BB651", "#191919"];
+    var colors = ["#D73131", "black", "#FA1159", "#FF162E", "#0068FF", "#2BB651", "#006BFF"];
     var pages = ["iteachyou.html", "spud.html?spud", "yip.html", "firestat.html", "donutfighter.html", "warriorsvoice.html", "routes.html"];
     
     $("#carousel_content").css("width", size + "00%");
